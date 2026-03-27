9f66daa7d0d4d66740b30785b15d08c9cad6ad03
Felipe Cordeiro <olifelipecordeiro@gmail.com>
2026-03-27 13:51:26 -0300

    Feat: Add tabela aluno

---
+++ main.py
@@ -0,0 +1,27 @@
+# Relacionamentos Muitos para Muitos (N:N)
+
+# Estudantes se inscrevem em cursos.
+# Um estudante pode fazer vários cursos.
+# um curso pode ter vários estudantes
+
+# Forma simples:
+# A relação não precisa guardar dados extres
+# So fazer o relacionamento
+
+from sqlalchemy import create_engine, Column, Integer, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+Base = declarative_base()
+
+
+# Tabelas curso e aluno
+class Aluno(Base):
+    __tablename__ = "alunos"
+
+    #Como cria uma coluna
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+
+    #Função para imprimir
+    def __repr__(self):
+        return f"ID: {self.id} - NOME: {self.nome}"
